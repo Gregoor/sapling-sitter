f77924f9a23ee38a312bf73174c1fc9d01788ad2
Gregor Weber <mail@dflate.io>
2023-03-10 22:48:18 +0100

    Shoopdaloop

--- src/main.rs
+++ src/main.rs
@@ -6,38 +6,26 @@ The async solution has that kind of implicitly where I just run of and try to re
 when they hit a symbol that can't be resolved. They would then continue whenever that given symbol has been resolved.
 */
 
-use std::{
-    collections::{hash_map::Entry, HashMap},
-    fs,
-    sync::Arc,
-};
+use std::{collections::HashMap, fs};
 
-use async_recursion::async_recursion;
-use tokio::{sync::RwLock, task::JoinSet};
+use crate::tree_sitter_cli::{parse_grammar::parse_grammar, rules::Rule};
 
-use crate::{
-    eventually::Eventually,
-    tree_sitter_cli::{parse_grammar::parse_grammar, rules::Rule},
-};
-
-mod eventually;
 mod tree_sitter_cli;
 
-type SymbolResolutions = RwLock<HashMap<String, Eventually<Option<String>>>>;
+type SymbolResolutions = HashMap<String, String>;
 
 fn quote(str: &str) -> String {
     format!("\"{str}\"")
 }
 
-#[async_recursion]
-async fn resolve_rule(
-    pattern_matches: HashMap<String, String>,
-    symbol_resolutions: Arc<SymbolResolutions>,
-    rule: Rule,
+fn resolve_rule(
+    pattern_matches: &HashMap<String, String>,
+    symbol_resolutions: &mut SymbolResolutions,
+    rule: &Rule,
 ) -> Option<String> {
     match rule {
         Rule::Blank => Some(String::new()),
-        Rule::String(s) => Some(s),
+        Rule::String(s) => Some(s.clone()),
         Rule::Pattern(s) => {
             if let Some(str) = pattern_matches.get(&s.clone()) {
                 return Some(str.clone());
@@ -47,16 +35,14 @@ async fn resolve_rule(
         }
         Rule::Metadata {
             params: _params,
-            rule,
-        } => resolve_rule(pattern_matches, symbol_resolutions, *rule).await,
+            rule: _rule,
+        } => None,
 
-        Rule::Repeat(rule) => resolve_rule(pattern_matches, symbol_resolutions, *rule).await,
+        Rule::Repeat(rule) => resolve_rule(pattern_matches, symbol_resolutions, rule),
         Rule::Seq(rules) => {
             let mut strings = vec![];
             for rule in rules {
-                if let Some(resolution) =
-                    resolve_rule(pattern_matches.clone(), symbol_resolutions.clone(), rule).await
-                {
+                if let Some(resolution) = resolve_rule(pattern_matches, symbol_resolutions, rule) {
                     strings.push(resolution);
                 } else {
                     return None;
@@ -65,37 +51,13 @@ async fn resolve_rule(
 
             Some(strings.join(""))
         }
-        Rule::Choice(rules) => {
-            let mut join_set = JoinSet::new();
+        Rule::Choice(rules) => rules
+            .into_iter()
+            .map(|rule| resolve_rule(pattern_matches, symbol_resolutions, rule))
+            .filter_map(|r| r)
+            .min(),
 
-            for rule in rules {
-                let symbol_resolutions = symbol_resolutions.clone();
-                let pattern_matches = pattern_matches.clone();
-                join_set.spawn(async move {
-                    resolve_rule(pattern_matches, symbol_resolutions, rule).await
-                });
-            }
-
-            while let Some(Ok(Some(s))) = join_set.join_next().await {
-                return Some(s);
-            }
-            None
-        }
-
-        Rule::NamedSymbol(name) => {
-            println!("⏳ Waiting for {} to resolve", quote(&name));
-            {
-                let mut symbol_resolution = symbol_resolutions.write().await;
-                symbol_resolution
-                    .entry(name.clone())
-                    .or_insert(Eventually::new());
-                // drop the write lock
-            }
-            let entry = symbol_resolutions.read().await;
-            let str = entry.get(&name).unwrap().read().await;
-            println!("🎉 Resolved {}", quote(&name));
-            return str;
-        }
+        Rule::NamedSymbol(name) => symbol_resolutions.get(name).map(|s| s.clone()),
         Rule::Symbol(sym) => {
             // TODO
             dbg!(sym);
@@ -104,13 +66,12 @@ async fn resolve_rule(
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // "../tree-sitter-typescript/typescript/src/grammar.json"
-    let grammar_str = fs::read_to_string("../tree-sitter-json/src/grammar.json")?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let grammar_str = fs::read_to_string("../tree-sitter-typescript/typescript/src/grammar.json")?;
     let grammar = parse_grammar(&grammar_str)?;
 
     let pattern_matches: HashMap<String, String> = HashMap::from([
+        // JSON
         (r#"[^\\"\n]+"#.into(), "".into()),
         (r#"(\"|\\|\/|b|f|n|r|t|u)"#.into(), "\"".into()),
         (r#"[^*]*\*+([^/*][^*]*\*+)*"#.into(), "*".into()),
@@ -118,48 +79,27 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         ("[0-7]+".into(), "0".into()),
         (r#"\d+"#.into(), "0".into()),
         (r#"[\da-fA-F]+"#.into(), "0".into()),
+        ("[1-9]".into(), "1".into()),
+        ("[0-1]+".into(), "0".into()),
+        // TypeScript
+        (
+            r#"[^\x00-\x1F\s\p{Zs}0-9:;`"'@#.,|^&<=>+\-*/\\%?!~()\[\]{}\uFEFF\u2060\u200B]|\\u[0-9a-fA-F]{4}|\\u\{[0-9a-fA-F]+\}"#.into(),
+            "T".into()
+        ),
+        (r#"[a-zA-Z_$][a-zA-Z\d_$]*-[a-zA-Z\d_$\-]*"#.into(), "A-".into()),
+        (".{1,}".into(), "T".into()),
+        ("[^{}<>]+".into(), "T".into()),
+        ("#!.*".into(), "#!".into())
     ]);
 
-    let symbol_resolutions: Arc<SymbolResolutions> = Default::default();
+    let mut symbol_resolutions: SymbolResolutions = Default::default();
 
-    let mut join_set = JoinSet::new();
-    println!(
-        "ℹ️ Resolving {} vars: {}",
-        grammar.variables.len(),
-        grammar
-            .variables
-            .iter()
-            .map(|v| v.name.clone())
-            .collect::<Vec<String>>()
-            .join(", ")
-    );
-    for var in grammar.variables {
-        let sr = symbol_resolutions.clone();
-        let pattern_matches = pattern_matches.clone();
-        join_set.spawn(async move {
-            println!("🧮 Resolving {}", quote(&var.name));
-            let resolution = resolve_rule(pattern_matches, sr.clone(), var.rule).await;
-            if let Some(s) = resolution.clone() {
-                println!("✅ Resolved {} to {:?}", quote(&var.name), s);
-            } else {
-                println!("🛑 Could not resolve {}", quote(&var.name));
+    for _n in 0..grammar.variables.len() {
+        for var in &grammar.variables {
+            if let Some(s) = resolve_rule(&pattern_matches, &mut symbol_resolutions, &var.rule) {
+                symbol_resolutions.insert(var.name.clone(), s);
             }
-
-            match sr.write().await.entry(var.name) {
-                Entry::Occupied(mut entry) => {
-                    entry.get_mut().write(resolution).await;
-                }
-                Entry::Vacant(entry) => {
-                    let eventually = Eventually::new();
-                    eventually.write(resolution).await;
-                    entry.insert(eventually);
-                }
-            };
-        });
-    }
-
-    loop {
-        dbg!(join_set.join_next().await);
+        }
     }
 
     dbg!(symbol_resolutions);
